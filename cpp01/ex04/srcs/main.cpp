145a3913818bd49676e0964b705445c0c33f432f
cviegas <cesairepro@gmail.com>
2024-05-23 19:54:03 +0200

    finished 4

--- cpp01/ex04/srcs/main.cpp
+++ cpp01/ex04/srcs/main.cpp
@@ -6,10 +6,23 @@
 
 void replace(std::ifstream &file, std::string filename, std::string s1, std::string s2)
 {
-	// Create a new file with the filename + ".replace"
-	std::ofstream newfile(filename + ".replace");
+	std::ofstream newfile((filename + ".replace").c_str());
 	if (!newfile)
 		return ((void)(std::cerr << "Unable to create file " << filename + ".replace" << std::endl));
+	std::string line;
+	while (std::getline(file, line))
+	{
+		line += '\n';
+		size_t pos;
+		while ((pos = line.find(s1)) != std::string::npos)
+		{
+			newfile << line.substr(0, pos);
+			newfile << s2;
+			line.erase(0, pos + s1.length());
+		}
+		newfile << line;
+	}
+	newfile.close();
 }
 
 int main(int ac, char **av)
